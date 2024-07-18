fb7b17803817e1cd938f3cb90243af6a6055f160
aaaflocksodsheep <sa5874@naver.com>
2024-07-18 17:23:14 +0900

    project start

---
+++ src/main/java/com/yang/shop/BasicController.java
@@ -0,0 +1,24 @@
+package com.yang.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/") // url 입력
+//    @ResponseBody
+    String hello (){   // 함수
+        return "index.html"; // HTML 파일 경로 적으면 된다
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String date(){
+        return ZonedDateTime.now().toString();
+    }
+ }
